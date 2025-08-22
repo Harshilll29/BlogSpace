caea94f20fb5f0009ab0780cd904549ee672f6e6
Harshilll29 <harshiljoshi525@gmail.com>
2025-08-22 10:27:18 +0530

    cors fix

--- server/server.js
+++ server/server.js
@@ -33,10 +33,12 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'http://localhost:3000', 
+    'http://localhost:3000',
     'https://blogsspace.netlify.app'
   ],
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 admin.initializeApp({
